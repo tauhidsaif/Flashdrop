ffeacd8ee607171dc19362f44e28486187504c09
MOHD TAUHID <83533970+tauhidsaif@users.noreply.github.com>
2025-07-29 10:16:53 +0530

    Update fileWorker.js

--- public/fileWorker.js
+++ public/fileWorker.js
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
+self.onmessage = function (e) {
+  const { file, offset, chunkSize } = e.data;
+  const reader = new FileReader();
+
+  reader.onload = (event) => {
+  const buffer = event.target.result;
+  self.postMessage(
+    {
+      buffer,
+      offset,
+    },
+    [buffer] // <-- ✅ Transfer instead of copy
+  );
+};
+
+  reader.onerror = (err) => {
+    self.postMessage({ error: err });
+  };
+
+  const chunk = file.slice(offset, offset + chunkSize);
+  reader.readAsArrayBuffer(chunk);
+};
+=======
 self.onmessage = function (e) {
   const { file, offset, chunkSize } = e.data;
   const reader = new FileReader();
@@ -16,3 +40,4 @@ self.onmessage = function (e) {
   const chunk = file.slice(offset, offset + chunkSize);
   reader.readAsArrayBuffer(chunk);
 };
+>>>>>>> 2a0bbd6b52bd6a699a23bbcb96bb7b602bc98890
